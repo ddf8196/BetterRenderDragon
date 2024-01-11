2f63a900e548b69b586bafb85479d71fae03837f
DDF <1711356221@qq.com>
2024-01-11 22:42:10 +0800

    Refactor code

--- BetterRenderDragon/MCPatches.cpp
+++ BetterRenderDragon/MCPatches.cpp
@@ -11,20 +11,16 @@ void MCPatches_Init() {
 		//1.19.40
 		uintptr_t ptr = FindSignature("83 BE ?? 02 00 00 65 ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? 83 BE ?? 02 00 00 65");
 		if (ptr) {
-			DWORD oldProtect, tmp;
-			VirtualProtect((void*)ptr, 60, PAGE_READWRITE, &oldProtect);
+			scopedVP(ptr, 60, PAGE_READWRITE);
 			((char*)ptr)[6] = 0x7F;
 			((char*)ptr)[59] = 0x7F;
-			VirtualProtect((void*)ptr, 60, oldProtect, &tmp);
 		} else {
 			//1.20.30.21 preview
 			uintptr_t ptr2 = FindSignature("83 39 65 ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? 83 39 65");
 			if (ptr2) {
-				DWORD oldProtect, tmp;
-				VirtualProtect((void*)ptr2, 60, PAGE_READWRITE, &oldProtect);
+				scopedVP(ptr2, 60, PAGE_READWRITE);
 				((char*)ptr2)[2] = 0x7F;
 				((char*)ptr2)[52] = 0x7F;
-				VirtualProtect((void*)ptr2, 60, oldProtect, &tmp);
 			} else {
 				printf("Failed to patch bgfx::d3d12rtx::RendererContextD3D12RTX::init\n");
 			}
@@ -40,11 +36,9 @@ void MCPatches_Init() {
 		ptr2 = FindSignature("81 BE ?? ?? 00 00 86 80 00 00");
 	}
 	if (ptr2) {
-		DWORD oldProtect, tmp;
-		VirtualProtect((void*)ptr2, 10, PAGE_READWRITE, &oldProtect);
+		scopedVP(ptr2, 10, PAGE_READWRITE);
 		((char*)ptr2)[6] = 0;
 		((char*)ptr2)[7] = 0;
-		VirtualProtect((void*)ptr2, 10, oldProtect, &tmp);
 	} else {
 		printf("Failed to patch bgfx::d3d12::RendererContextD3D12::init\n");
 	}
@@ -54,10 +48,8 @@ void MCPatches_Init() {
 	//1.20.0.22 preview
 	uintptr_t ptr3 = FindSignature("FF E1 B8 00 00 07 00 C3");
 	if (ptr3) {
-		DWORD oldProtect, tmp;
-		VirtualProtect((void*)ptr3, 8, PAGE_READWRITE, &oldProtect);
+		scopedVP(ptr3, 8, PAGE_READWRITE);
 		((char*)ptr3)[5] = 0;
-		VirtualProtect((void*)ptr3, 8, oldProtect, &tmp);
 	} else {
 		printf("Failed to patch dragon::bgfximpl::toSamplerFlags\n");
 	}
@@ -66,19 +58,15 @@ void MCPatches_Init() {
 	//1.20.30.02
 	uintptr_t ptr4 = FindSignature("83 FB 01 75 11 48 8B 01 48 8B 40 40 FF 15 ? ? ? ? 32 C0 EB 02");
 	if (ptr4) {
-		DWORD oldProtect, tmp;
-		VirtualProtect((void*)ptr4, 22, PAGE_READWRITE, &oldProtect);
+		scopedVP(ptr4, 22, PAGE_READWRITE);
 		((char*)ptr4)[18] = 0xB0;
 		((char*)ptr4)[19] = 0x01;
-		VirtualProtect((void*)ptr4, 22, oldProtect, &tmp);
 	} else {
 		//1.20.30.20 preview
 		ptr4 = FindSignature("83 FB 01 75 1A 48 8B 01 48 8B 40 40 FF 15 ? ? ? ? 84 C0 74 05 45 84 E4 75 04");
 		if (ptr4) {
-			DWORD oldProtect, tmp;
-			VirtualProtect((void*)ptr4, 27, PAGE_READWRITE, &oldProtect);
+			scopedVP(ptr4, 27, PAGE_READWRITE);
 			((char*)ptr4)[3] = 0x74;
-			VirtualProtect((void*)ptr4, 27, oldProtect, &tmp);
 		} else {
 			printf("Failed to patch MinecraftGame::_updateLightingModel\n");
 		}
--- BetterRenderDragon/MCPatches.h
+++ BetterRenderDragon/MCPatches.h
@@ -1,3 +1,21 @@
 #pragma once
 
 void MCPatches_Init();
+
+class ScopedVirtualProtect {
+public:
+    ScopedVirtualProtect(void* addr, size_t size, DWORD newProtect) : addr(addr), size(size) {
+        VirtualProtect(addr, size, newProtect, &oldProtect);
+    }
+    ~ScopedVirtualProtect() {
+        VirtualProtect(addr, size, oldProtect, &oldProtect);
+    }
+private:
+    void* addr;
+    size_t size;
+    DWORD oldProtect;
+};
+
+#define glue1(a, b) a##b
+#define glue(a, b) glue1(a,b)
+#define scopedVP(ptr, size, newProtect) ScopedVirtualProtect glue(svp,__LINE__) ((void*)(ptr), (size), (newProtect));
